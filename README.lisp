fa55a47fc022110316132b74cd2a22ae87f7edba
Snowmanzzz(Zhengzhong Zhao) <zhengzhongzhao@gmail.com>
2019-03-07 22:01:55 +0800

    Update README.lisp

--- README.lisp
+++ README.lisp
@@ -1,4 +1,8 @@
-
+ ;; First try.  If the current element is a leaf, cons it to the fringe 
+ ;; of the rest.  If the current element is a tree, cons the fringe if 
+ ;; it to the fringe of the rest.  .... Won't work because the bad line 
+ ;; indicated won't build a flat list, it will always end in nested 
+ ;; cons. 
  (define (fringe tree) 
    (define nil '()) 
    (if (null? tree)  
@@ -8,3 +12,4 @@
              (cons first (fringe (cdr tree))) 
              ;; bad line follows: 
              (cons (fringe first) (fringe (cdr tree))))))) 
+  
