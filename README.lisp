71c0636189b39b5e84c66cf83a751c2c0a255ad4
Snowmanzzz(Zhengzhong Zhao) <zhengzhongzhao@gmail.com>
2019-03-07 21:56:53 +0800

    Update README.lisp

--- README.lisp
+++ README.lisp
@@ -1,1 +1,10 @@
 
+ (define (fringe tree) 
+   (define nil '()) 
+   (if (null? tree)  
+       nil 
+       (let ((first (car tree))) 
+         (if (not (pair? first)) 
+             (cons first (fringe (cdr tree))) 
+             ;; bad line follows: 
+             (cons (fringe first) (fringe (cdr tree))))))) 
